de8071f960c48ad265ad69803275bcb1b2d4f4e8
abreukuse <t.kuse6@gmail.com>
2021-03-09 20:21:22 -0300

    Pipeline version support

--- rossmann_store_sales/training.py
+++ rossmann_store_sales/training.py
@@ -2,20 +2,32 @@ import pandas as pd
 import numpy as np
 import joblib
 from rossmann_store_sales import config, pipeline
+from rossmann_store_sales import __version__ as _version
 
 def save_pipeline(*, pipeline_to_persist) -> None:
     """Persist the pipeline."""
-    save_file_name = 'pipeline_010.pkl'
+    save_file_name = f"pipeline_version_{_version.replace('.','-')}.pkl"
     save_path = config.TRAINED_MODEL_DIR / save_file_name
+
+    remove_old_pipelines(file_to_keep=save_file_name)
     joblib.dump(pipeline_to_persist, save_path)
-    print('Pipeline saved.')
+    print(f'Pipeline version {_verion} saved.')
+
+
+def remove_old_pipelines(*, file_to_keep):
+    """Delete old model pipelines from the package"""
+    for model_file in config.TRAINED_MODEL_DIR.iterdir():
+        if model_file.name not in [file_to_keep, '__init__.py']:
+            model_file.unlink()
+
 
 def apply_pipeline_steps() -> None:
     """Apply all the pipeline stesps in the training data"""
     train = config.TRAIN
     train_preprocessed = pipeline.pipeline.fit_transform(train)
-    save_path = config.DATASET_DIR / 'train_preprocessed.csv'
-    train_preprocessed.to_csv(save_path, index=False)
+    save_path = config.DATASET_DIR / f'train_preprocessed_{_version}.csv'
+    # Save just a sample of the prepreprossed data
+    train_preprocessed.head(10).to_csv(save_path, index=False)
     print('Training data preprocessed was saved.')
 
     save_pipeline(pipeline_to_persist=pipeline.pipeline)
