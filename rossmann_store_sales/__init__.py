ed0e1e22ed4fbde1bdd3531630d1745005f3b29d
abreukuse <t.kuse6@gmail.com>
2021-03-09 20:20:36 -0300

    Versioning

--- rossmann_store_sales/__init__.py
+++ rossmann_store_sales/__init__.py
@@ -1,1 +1,6 @@
- 
+from rossmann_store_sales import config
+
+VERSION_PATH = config.PACKAGE_ROOT / 'VERSION'
+
+with open(VERSION_PATH, 'r') as version_file:
+    __version__ = version_file.read().strip()
